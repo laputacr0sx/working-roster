e78094315a5ef5e5d5c4b7df2555d09fc9a4b218
Veilics <laputafelix@gmail.com>
2020-10-29 15:35:12 +0800

    X72 timetable added

--- get_duty.py
+++ get_duty.py
@@ -1,7 +1,23 @@
-import requests
+import datetime as dt
+import time as time
+import os
 
-url = "www.52jushuang.com"
 
-res = requests.get(url)
+raw = {
+    'M009': (dt.time(5,38), dt.time(6,18)),
+    'M046': (dt.time(6,38), dt.time(7,20)),
+    'M071': (dt.time(7,31), dt.time(8,16)),
+    'M118': (dt.time(8,33), dt.time(9,15)),
+    'M161': (dt.time(10,37), dt.time(11,34)),
+    'M182': (dt.time(11,59), dt.time(12,43))      
+}
 
-print(res)
+tn = list(raw.keys())
+schedule = list(raw.values())
+
+td = dt.datetime(1,1,1)
+for i in raw.keys():
+    k = raw[i]
+
+with open("mtr_duty.json", 'r', encoding="utf-8") as duty:
+    print(duty)
