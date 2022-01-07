435241e804c2e73792194dbecd43b94f21361e07
veilicsVV <laputafelix@gmail.com>
2022-01-08 02:35:25 +0800

    duty updated

---
+++ gg.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df = pd.DataFrame(
+    {"a" : [4 ,5, 6],"b":[7,8,9],"c" : [10, 11, 12]},index = pd.MultiIndex.from_tuples(
+[('d', 1), ('d', 2),('e', 2)], names=['n', 'v']))
+
+print(df)
